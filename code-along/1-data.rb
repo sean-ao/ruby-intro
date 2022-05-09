e3dcf24281660a7536ddc5fed4524022550193d2
sean-ao <sean.augustine-obi@kellogg.northwestern.edu>
2022-05-09 06:03:51 +0000

    .

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -1,19 +1,22 @@
 # To run this code, be sure your current working directory
 # is the same as where this file is located and then run:
 # ruby 1-data.rb
-
+puts "Hello, world!"
 # In Ruby, there are different *types* of data:
 
 # Numbers
 
 # Perform simple math with numbers
-
+puts 5+2
 # Strings
-
+puts "tacos"
+puts "tacos" * 3
 # Combine strings together
 
 # Variables
-
+quantity = 3 + 3 + 3
+food = "tacos"
+puts quantity - 4
 # Combine strings and variables
 
 # String manipulation
